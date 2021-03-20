cf036153f8bdfb389f6f7238ece4e5138a606baf
Nant1ng <andressantana99@hotmail.se>
2021-03-20 20:11:23 +0100

    Read all products

---
+++ objects/Products.php
@@ -0,0 +1,44 @@
+<?php
+    class Products{
+        // Database
+        private $conn;
+        private $table = 'products';
+
+        // Properties
+        public $id;
+        public $title;
+        public $description;
+        public $imgUrl;
+        public $price;
+        public $created_at;
+        public $category_id;
+
+        // Constructor 
+        public function __construct($db){
+            $this->conn = $db;
+        }
+
+        // Get products
+        public function read(){
+            $query = 'SELECT
+                     c.name AS category_name,
+                     p.id, 
+                     p.title,
+                     p.description,
+                     p.imgUrl,
+                     p.price,
+                     p.created_at, 
+                     p.category_id
+                    FROM
+                    ' . $this->table . ' p
+                    LEFT JOIN
+                     categories c ON p.category_id = c.id
+                    ORDER BY
+                     p.created_at DESC';
+
+            $stm = $this->conn->prepare($query);
+            $stm->execute();
+            return $stm;
+        }
+    }
+?>
