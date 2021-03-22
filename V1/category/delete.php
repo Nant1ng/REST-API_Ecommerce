e65bef1c0484be76267b92b8466d70cee55b08c1
Nant1ng <andressantana99@hotmail.se>
2021-03-22 19:27:59 +0100

    Delete Category

--- V1/category/delete.php
+++ V1/category/delete.php
@@ -8,27 +8,25 @@
 
     include_once '../../config/Database.php';
     include_once '../../objects/Category.php';
-    // Instantiate DB & connect
+
     $database = new Database();
     $db = $database->connect();
 
-    // Instantiate blog post object
     $category = new Category($db);
 
     // Get raw posted data
     $data = json_decode(file_get_contents("php://input"));
 
-    // Set ID to UPDATE
     $category->id = $data->id;
 
     // Delete category
     if($category->delete()) {
         echo json_encode(
-        array('message' => 'Category deleted')
+            array('message' => 'Category deleted')
         );
     } else {
         echo json_encode(
-        array('message' => 'Category Not Deleted')
+            array('message' => 'Category Not Deleted')
         );
     }
 ?>
